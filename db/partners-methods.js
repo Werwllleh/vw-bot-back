59077a7d3666514753fba953d6d72072deb0df7d
Alexey A <allvxm4@gmail.com>
2024-11-28 19:07:53 +0300

    DB belong to many associations

--- db/partners-methods.js
+++ db/partners-methods.js
@@ -1,4 +1,4 @@
-import {Partners, PartnersCategories} from '../models.js';
+import {PartnerCategoryConnect, Partners, PartnersCategories} from '../models.js';
 import {getUserInfo} from "./user-methods.js";
 import {getRandomColor} from "../functions/randomColor.js";
 import {translite} from "../functions/translite.js";
@@ -61,44 +61,93 @@ export const createPartner = async (chatId, data) => {
   try {
     const userInfo = await getUserInfo(chatId);
 
-    if (!userInfo.user_admin) {
-      return false;
-    }
+    // console.log(data)
+    const categoriesIdArr = data.categories;
 
-    // Создаем партнера
-    const partner = await Partners.create({
+    const createPartner = await Partners.create({
       title: data.title,
       description: data.description,
+      categories: JSON.stringify(categoriesIdArr),
       links: JSON.stringify(data.links),
       phones: JSON.stringify(data.phones),
       address_text: data.address_text,
       address_coordinates: JSON.stringify(data.address_coordinates),
+      status: false
     });
 
-    // Добавляем категории
-    for (const categoryValue of data.categories) {
-      const partnerCategory = await PartnersCategories.findOne({
-        where: { value: categoryValue },
+    await Partners.findByPk(createPartner.id)
+      .then(partner => {
+        if(!partner) return;
+
+        categoriesIdArr.map(categoryId => {
+          PartnersCategories.findByPk(categoryId)
+          .then(category => {
+
+            partner.addPartnersCategories(category);
+          })
+        })
+
+      })
+
+    /*Student.findOne({where: {name: "Tom"}})
+      .then(student=>{
+        if(!student) return;
+
+        // добавим Тому курс по JavaScript
+        Course.findOne({where: {name: "JavaScript"}})
+          .then(course=>{
+            if(!course) return;
+            student.addCourse(course, {through:{grade:1}});
+          });
+      });*/
+
+    /*await Student.findOne({where: {name: "Tom"}})
+      .then(student=>{
+        if(!student) return;
+        student.getCourses().then(courses=>{
+          courses.map(course => {
+            console.log(course.name);
+          })
+          /!*for(course of courses){
+
+          }*!/
+        });
+      });*/
+
+
+
+    /*if (userInfo.user_admin) {
+      await Partners.create({
+        title: data.title,
+        description: data.description,
+        categories: JSON.stringify(data.categories),
+        links: JSON.stringify(data.links),
+        phones: JSON.stringify(data.phones),
+        address_text: data.address_text,
+        address_coordinates: JSON.stringify(data.address_coordinates),
+        status: true
       });
+    } else {
+      await Partners.create({
+        title: data.title,
+        description: data.description,
+        categories: JSON.stringify(data.categories),
+        links: JSON.stringify(data.links),
+        phones: JSON.stringify(data.phones),
+        address_text: data.address_text,
+        address_coordinates: JSON.stringify(data.address_coordinates),
+        status: false
+      });
+    }*/
 
-      if (!partnerCategory) {
-        console.error(`Категория с value "${categoryValue}" не найдена.`);
-        continue; // Пропустить эту категорию
-      }
+    /*
+    const category = await PartnersCategories.findByPk(categoryId);
 
-      await partner.addPartnersCategory(partnerCategory);
-    }
+    */
 
-    // Возвращаем партнера с категориями
-    const result = await Partners.findOne({
-      where: { id: partner.id },
-      include: {
-        model: PartnersCategories,
-        through: { attributes: [] },
-      },
-    });
 
-    return result;
+
+    return true;
   } catch (err) {
     console.error('Ошибка при добавлении партнера', err);
     throw err; // Для дальнейшей обработки ошибки
--- models.js
+++ models.js
@@ -1,66 +1,120 @@
 import sequelize from './db.js';
-import { DataTypes } from 'sequelize';
+import {DataTypes} from 'sequelize';
 
 export const Users = sequelize.define('users', {
-	id: { type: DataTypes.INTEGER, primaryKey: true, unique: true, autoIncrement: true },
-	chat_id: { type: DataTypes.BIGINT, unique: true },
-	user_name: { type: DataTypes.STRING, allowNull: false },
-	user_color: { type: DataTypes.STRING, allowNull: false },
-	user_admin: { type: DataTypes.BOOLEAN, allowNull: false, defaultValue: false },
+  id: {type: DataTypes.INTEGER, primaryKey: true, unique: true, autoIncrement: true},
+  chat_id: {type: DataTypes.BIGINT, unique: true},
+  user_name: {type: DataTypes.STRING, allowNull: false},
+  user_color: {type: DataTypes.STRING, allowNull: false},
+  user_admin: {type: DataTypes.BOOLEAN, allowNull: false, defaultValue: false},
 }, {
-	timestamps: true
+  timestamps: true
 });
 
 export const Cars = sequelize.define('cars', {
-	id: { type: DataTypes.INTEGER, primaryKey: true, unique: true, autoIncrement: true },
-	car_brand: { type: DataTypes.STRING, allowNull: false },
-	car_model: { type: DataTypes.STRING, allowNull: false },
-	car_year: { type: DataTypes.INTEGER, allowNull: false },
-	car_number: { type: DataTypes.STRING, allowNull: false, unique: true },
-	car_note: { type: DataTypes.TEXT },
-	car_images: { type: DataTypes.TEXT, allowNull: false }, // Для хранения названий фотографий
+  id: {type: DataTypes.INTEGER, primaryKey: true, unique: true, autoIncrement: true},
+  car_brand: {type: DataTypes.STRING, allowNull: false},
+  car_model: {type: DataTypes.STRING, allowNull: false},
+  car_year: {type: DataTypes.INTEGER, allowNull: false},
+  car_number: {type: DataTypes.STRING, allowNull: false, unique: true},
+  car_note: {type: DataTypes.TEXT},
+  car_images: {type: DataTypes.TEXT, allowNull: false}, // Для хранения названий фотографий
 }, {
-	timestamps: true
+  timestamps: true
 });
 
 
 // Установка связи между Users и Cars
 Users.hasMany(Cars, {
-	foreignKey: 'chat_id',
-	sourceKey: 'chat_id'
+  foreignKey: 'chat_id',
+  sourceKey: 'chat_id'
 });
 
 Cars.belongsTo(Users, {
-	foreignKey: 'chat_id',
-	targetKey: 'chat_id'
+  foreignKey: 'chat_id',
+  targetKey: 'chat_id'
 });
 
 
 export const Partners = sequelize.define('partners', {
-	id: { type: DataTypes.INTEGER, primaryKey: true, unique: true, autoIncrement: true },
-	title: { type: DataTypes.STRING, allowNull: false },
-	description: { type: DataTypes.STRING, allowNull: false },
-	links: { type: DataTypes.TEXT, allowNull: true },
-	phones: { type: DataTypes.TEXT, allowNull: true },
-	address_text: { type: DataTypes.STRING, allowNull: false },
-	address_coordinates: { type: DataTypes.TEXT, allowNull: false },
-	// category: { type: DataTypes.INTEGER, allowNull: false },
-	images: { type: DataTypes.TEXT, allowNull: true },
-	status: { type: DataTypes.BOOLEAN, allowNull: false, defaultValue: false },
+  id: {type: DataTypes.INTEGER, primaryKey: true, unique: true, autoIncrement: true},
+  categories: {type: DataTypes.TEXT, allowNull: false},
+  title: {type: DataTypes.STRING, allowNull: false},
+  description: {type: DataTypes.STRING, allowNull: false},
+  links: {type: DataTypes.TEXT, allowNull: true},
+  phones: {type: DataTypes.TEXT, allowNull: true},
+  address_text: {type: DataTypes.STRING, allowNull: false},
+  address_coordinates: {type: DataTypes.TEXT, allowNull: false},
+  images: {type: DataTypes.TEXT, allowNull: true},
+  status: {type: DataTypes.BOOLEAN, allowNull: false, defaultValue: false},
 }, {
-	timestamps: true
+  timestamps: true
 });
 
-export const PartnersCategories = sequelize.define('partners_categories', {
-	id: { type: DataTypes.INTEGER, primaryKey: true, unique: true, autoIncrement: true },
-	label: { type: DataTypes.STRING, allowNull: false, unique: true },
-	value: { type: DataTypes.STRING, allowNull: false, unique: true },
+export const PartnersCategories = sequelize.define('partnersCategories', {
+  id: {type: DataTypes.INTEGER, primaryKey: true, unique: true, autoIncrement: true},
+  label: {type: DataTypes.STRING, allowNull: false, unique: true},
+  value: {type: DataTypes.STRING, allowNull: false, unique: true},
 }, {
-	timestamps: false
+  timestamps: false
 })
 
-// Промежуточная таблица создается автоматически Sequelize
-Partners.belongsToMany(PartnersCategories, { through: 'PartnersCategoriesPartners' });
-PartnersCategories.belongsToMany(Partners, { through: 'PartnersCategoriesPartners' });
+// Промежуточная таблица
+export const PartnerCategoryConnect = sequelize.define('partnerCategoryConnect', {
+  id: {
+    type: DataTypes.INTEGER,
+    autoIncrement: true,
+    primaryKey: true,
+    allowNull: false
+  },
+}, {
+  timestamps: false,
+});
 
+// Настройка ассоциаций
+Partners.belongsToMany(PartnersCategories, {through: PartnerCategoryConnect});
+PartnersCategories.belongsToMany(Partners, {through: PartnerCategoryConnect});
+
+
+/*export const Student = sequelize.define("student", {
+  id: {
+    type: DataTypes.INTEGER,
+    autoIncrement: true,
+    primaryKey: true,
+    allowNull: false
+  },
+  name: {
+    type: DataTypes.STRING,
+    allowNull: false
+  }
+});
+
+export const Course = sequelize.define("course", {
+  id: {
+    type: DataTypes.INTEGER,
+    autoIncrement: true,
+    primaryKey: true,
+    allowNull: false
+  },
+  name: {
+    type: DataTypes.STRING,
+    allowNull: false
+  }
+});
+
+// промежуточная сущность, которая связывает курс и студента
+export const Enrolment = sequelize.define("enrolment", {
+  id: {
+    type: DataTypes.INTEGER,
+    autoIncrement: true,
+    primaryKey: true,
+    allowNull: false
+  }
+},
+  {
+    timestamps: false
+  });
+
+Student.belongsToMany(Course, {through: Enrolment});
+Course.belongsToMany(Student, {through: Enrolment});*/
 
